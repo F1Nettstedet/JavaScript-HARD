919adca324326ad8cc328a1604e20e7b1b825186
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-07 23:10:50 +0500

    Выполнение усложненного 4 задани

--- sriptHard.js
+++ sriptHard.js
@@ -1,31 +1,23 @@
 'use strict'
+// Задача №1 
 
-//Задача №1 
+const changingString = function(changing){
 
-let lang = prompt("На каком языке вывести дни недели 'ru' или 'eng'")
-let rusDayWeek = "Понедельник,вторник,среда,четверг,пятница,суббота,воскресенье"
-let engDayWeek = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
-
-if(lang === "ru"){
-    console.log(rusDayWeek)
-} else{
-    console.log(engDayWeek)
-}
-
-switch (lang){ 
-    case "ru": console.log(rusDayWeek);
-    break;
-    case "eng": console.log(engDayWeek)
-    break;
-
-}
-
-let arrDayWeek = [[rusDayWeek],[engDayWeek]]
-console.log(lang === "ru" ? arrDayWeek[0] : arrDayWeek[1])
-
-
-//Задача №2
-
-let namePerson = prompt('Введите имя')
-let role = namePerson === "Артем" ? 'Директор' : namePerson ==='Александр' ? "Преподаватель" : "Студент"
-console.log(role)
+    let result = '' 
+    let count = 0
+    
+    if(!isNaN(changing) && changing !== String()){
+     return alert('Передайте строку!')
+    }
+     result = changing.trim()
+    for(let i = 0; result.length > i; i++){
+     count = i
+    }
+    if(count > 30){
+     result =  result.slice(0,31) + '...'
+    }
+    return result
+    }
+    
+    
+   console.log(changingString(''))
