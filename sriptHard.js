5dde905852e40281103f7a937064034d54a8b6e3
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-12 14:37:12 +0500

    Решение задачи с помощью тернарного оператора

--- sriptHard.js
+++ sriptHard.js
@@ -7,14 +7,10 @@ let now = new Date()
 let nowDat = now.getDay()
 let days = ["Воскресенье", "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
 
-for (day in days){ 
-if(days[day] === days[6] || days[day] === days[0] && days[day] !== days[nowDat]){ 
-    list.innerHTML += "<li>" + '<i>' + days[day] 
-}
-else if(days[nowDat] == days[day]){
-    list.innerHTML += "<li>" + '<b>' + days[day] 
-}
-else{
-    list.innerHTML += "<li>"  + days[day]
-}
+for (day in days){
+    days[day] === days[6] || days[day] === days[0] && days[day] !== days[nowDat] ? 
+
+    list.innerHTML += "<li>" + '<i>' + days[day] : days[nowDat] == days[day] ? 
+    
+    list.innerHTML += "<li>" + '<b>' + days[day] : list.innerHTML += "<li>"  + days[day]
 }
