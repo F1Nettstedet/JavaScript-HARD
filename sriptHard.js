f3c4814d91bf853306adbbc9acb8a14a391ac8a9
Nikita Ilinykh <Mussolq@gmail.com>
2023-11-13 21:50:25 +0500

    Решение усложненоого задания №8

--- sriptHard.js
+++ sriptHard.js
@@ -1,16 +1,138 @@
-'use strict'
-// Задача №1 
+'use srtict'
+//Задача №1 Строка №67  способ решения через Reduce
 
-const list = document.getElementById('week')
-let day = 0
-let now = new Date()
-let nowDat = now.getDay()
-let days = ["Воскресенье", "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
+const appData = {
 
-for (day in days){
-    days[day] === days[6] || days[day] === days[0] && days[day] !== days[nowDat] ? 
+    title: '',
+    screens: [],
+    screenPrice: 0,
+    adaptive: true,
+    rollback: 50,
+    allServicePrices: 0,
+    fullPrice: 0,
+    servicePercentPrice: 0,
+    services: {},
 
-    list.innerHTML += "<li>" + '<i>' + days[day] : days[nowDat] == days[day] ? 
-    
-    list.innerHTML += "<li>" + '<b>' + days[day] : list.innerHTML += "<li>"  + days[day]
+    isString: function(str){ 
+        return isNaN(str)
+    },
+
+    isNumber: function (num) {
+        return !isNaN(parseFloat(num) && isFinite(num))
+    },
+
+    asking: function () {
+        let name = ''
+        let price = 0
+
+        do {
+            appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
+        } while(this.isNumber(this.title))
+
+        for (let i = 0; i < 2; i++) {
+            do {
+                name = prompt('Какие типы экранов нужно разработать?', 'Пример: Простые, Сложные, Интерактивные')
+            } while(this.isNumber(name))
+
+            do {
+                price = prompt('Сколько будет стоить данная работа?')
+            }
+            while (!appData.isNumber(price))
+
+            appData.screens.push({ id: i, name: name, price: price})
+
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let name = ''
+            let price = 0
+
+            do {
+                name = prompt('Какой допольнительный тип услуги нужен ?')
+            } while(this.isNumber(name))
+            
+
+            do {
+                price = prompt('Сколько это будет стоить?')
+            } while (!appData.isNumber(price))
+
+            appData.services[name + i] = +price   // по-другому что-то пока не дошло как  сделать так,чтобы при передаче в объект одинакового ключа не было перезаписи =(
+        }
+
+        appData.adaptive = confirm('Нужен ли адаптив на сайте?')
+    },
+
+    addPrices: function () {
+        
+        appData.screenPrice  = this.screens.reduce(function(acc,i){
+            return acc +  (+i.price)
+
+        },0)
+        
+
+        for (key in appData.services) {
+            appData.allServicePrices += this.services[key]
+        }
+
+    },
+
+
+    getFullPrice: function () {
+        appData.fullPrice = (appData.screenPrice + appData.allServicePrices)
+    },
+
+    getServicePercentPrices: function () {
+        appData.servicePercentPrice = (appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)))
+
+    },
+
+    getTitle: function () {
+        appData.title = (appData.title.trim()[0].toUpperCase() + appData.title.slice(1).toLowerCase())
+    },
+
+    getRollbackMessage: function (price) {
+        if (price >= 30000) {
+            return 'Даем скидку в 10%'
+        }
+
+        if (price >= 15000 && price < 30000) {
+            return 'Даем скидку в 5%'
+        }
+
+        if (price < 15000 && price >= 0) {
+            return 'Скидка не предусмотрена'
+        } else {
+            return 'Что-то пошло не так'
+        }
+
+    },
+    logger: function () {
+        console.log(appData.screenPrice)
+        console.log(this.fullPrice)
+        console.log(this.servicePercentPrice)
+        console.log(appData.screens)
+        console.log(appData.services)
+        for (key in appData) {
+            if (typeof appData[key] == 'function') {
+                console.log('Метод: ' + key)
+            } else {
+                console.log('Свойство: ' + key)
+            }
+        }
+
+
+    },
+
+    start: function () {
+        appData.asking()
+        appData.addPrices()
+        appData.getFullPrice()
+        appData.getServicePercentPrices()
+        appData.getTitle()
+        appData.logger()
+        
+    },
+
 }
+
+appData.start()
